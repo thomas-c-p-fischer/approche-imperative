24b094f1b79c5b7563701e3072510809c5ed0c3d
thomas.fischer <thomas.fischer@sully-group.com>
2023-10-04 14:22:36 +0200

    les variables sont déclarées.

---
+++ src/fr/declaration/variable/DeclaratioApp.java
@@ -0,0 +1,22 @@
+package fr.declaration.variable;
+
+public class DeclaratioApp {
+
+	public static void main(String[] args) {
+		byte petitNombre = 15;
+		short moyenNombre = 5000;
+		int nombre = 70000;
+		long grandNombre = 8000000000L;
+		float decimal = 1.2F;
+		double autreDecimal = 12.897;
+		char lettre = 'a';
+		boolean vrai = true;
+		String phrase = "une phrase";
+		String randomString = "Voici le résultat d'un calcul :\n 1+5=6";
+		
+		System.out.println(randomString);
+		
+
+	}
+
+}
