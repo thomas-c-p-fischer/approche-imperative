def3b316d837f4666956f50e84878400ca0d47a7
thomas.fischer <thomas.fischer@sully-group.com>
2023-10-05 12:27:24 +0200

    correction nom de class

--- src/fr/algorithmie/comparaisonTableau.java
+++ src/fr/algorithmie/comparaisonTableau.java
@@ -1,6 +1,6 @@
 package fr.algorithmie;
 
-public class comparaisonTableau {
+public class ComparaisonTableau {
 
 	public static void main(String[] args) {
 		int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4};
